78c9860f204311c9385b6c953dfdfa253a4c7870
Yannick Roffin <yannick.roffin@soprasteria.com>
2019-10-31 10:33:19 +0100

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BagagerieComponent } from './components/frames/bagagerie/bagagerie.component';
 
+import { CardModule } from 'primeng/card';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { TabViewModule } from 'primeng/tabview';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +18,12 @@ import { BagagerieComponent } from './components/frames/bagagerie/bagagerie.comp
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardModule,
+    ToolbarModule,
+    ButtonModule,
+    SplitButtonModule,
+    TabViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
